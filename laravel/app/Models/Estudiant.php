478458aa5c07ec56521c41f12ad50e1686653edd
Gerard Revert <gerardrevert@gmail.com>
2026-06-21 23:27:03 +0200

    models controllers requests tests migracions

---
+++ laravel/app/Models/Estudiant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estudiant extends Model
+{
+    /** @use HasFactory<\Database\Factories\EstudiantFactory> */
+    use HasFactory;
+}
